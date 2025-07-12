f2a8a4f7fa40e81eb45367c223115025e0711493
ryancrazyfr <ryancrazyfr@gmail.com>
2025-07-12 12:01:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,14 +38,13 @@ GROUP_IDS = [
 
 REMINDER_GROUP_ID = -1001664882105
 
-pop_links = """🔗 *Use these links for POP :*
-
-- Sexy Baddies: https://t.me/+tGBn9q_6Z-9jMTAx  
-- Content Hub: https://t.me/+F_BNXoMjPPhmNGEx  
-- Seductive Sirens: https://t.me/+nvm1zwZz7FA1MTdh  
-- The Sluts Store: https://t.me/+pkxiRKn2ZvcvMjI8  
-- My Hot Friends: https://t.me/+A47SCYO9z_MzOTcx  
-- CumSlut Paradise: https://t.me/+y5TaJPgVGvI1NzQ0  
+pop_links = """🔗 <b>Use these links for POP :</b><br><br>
+<b>Sexy Baddies</b><br><a href="https://t.me/+tGBn9q_6Z-9jMTAx">t.me/+tGBn9q_6Z-9jMTAx</a><br><br>
+<b>Content Hub</b><br><a href="https://t.me/+F_BNXoMjPPhmNGEx">t.me/+F_BNXoMjPPhmNGEx</a><br><br>
+<b>Seductive Sirens</b><br><a href="https://t.me/+nvm1zwZz7FA1MTdh">t.me/+nvm1zwZz7FA1MTdh</a><br><br>
+<b>The Sluts Store</b><br><a href="https://t.me/+pkxiRKn2ZvcyMjI8">t.me/+pkxiRKn2ZvcyMjI8</a><br><br>
+<b>My Hot Friends</b><br><a href="https://t.me/+A47SCYOy2_MzOTcx">t.me/+A47SCYOy2_MzOTcx</a><br><br>
+<b>CumSlut Paradise</b><br><a href="https://t.me/+y5TaJPgVGvI1NzQ0">t.me/+y5TaJPgVGvI1NzQ0</a>
 """
 
 if not os.path.exists(POP_DIR):
@@ -150,15 +149,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     await update.message.reply_markdown(welcome_msg)
           
-    pop_links = """🔗 *Use these links for POP :*
-
-- Sexy Baddies: https://t.me/+tGBn9q_6Z-9jMTAx  
-- Content Hub: https://t.me/+F_BNXoMjPPhmNGEx  
-- Seductive Sirens: https://t.me/+nvm1zwZz7FA1MTdh  
-- The Sluts Store: https://t.me/+pkxiRKn2ZvcvMjI8  
-- My Hot Friends: https://t.me/+A47SCYO9z_MzOTcx  
-- CumSlut Paradise: https://t.me/+y5TaJPgVGvI1NzQ0  
-"""
+    
     await update.message.reply_text(pop_links, parse_mode="HTML", disable_web_page_preview=True)
           
 
