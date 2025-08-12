5d43f23443b9ea6f33e7d111e9f6e1e506021566
ryancrazyfr <ryancrazyfr@gmail.com>
2025-08-12 14:08:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -530,7 +530,7 @@ async def mute_non_submitters_tuesday(context: ContextTypes.DEFAULT_TYPE):
                             )
                                          
                         )   
-                    await context.bot.send_message(
+                      await context.bot.send_message(
                           chat_id=user_id,
                           text="🔇 You’ve been muted in Tuesday pop groups. Please send your pop to get unmuted!"
                         )
